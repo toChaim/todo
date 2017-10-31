91328240a48b2062ec06493298c3e395e4ce9654
toChaim <toChaim@gmail.com>
2017-10-31 12:24:23 -0700

    main page rendering.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-var http = require('http');
+import express from "express";
+import path from "path";
 
-var server = http.createServer((req, res, next) => {
-  res.writeHead(200, { 'Content-type': 'text/json' });
-  res.write('<h1>Hello World!</h1>');
-  res.end();
+var root = { root: path.dirname("app.js") };
+var app = express();
+
+app.get("/", (req, res, next) => {
+  res.sendFile("index.html", root);
 });
 
-server.listen(3000, () => {
-  console.log('start linstening port 3000');
+app.get("/index.js", (req, res, next) => {
+  res.sendFile("index.js", root);
+});
+
+app.get("/index.css", (req, res, next) => {
+  res.sendFile("index.css", root);
+});
+
+app.get("/new_route/:name", (req, res, next) => {
+  console.log(req);
+  res.send(`Hi ${req.params.name}`);
+});
+
+app.listen(3000, () => {
+  console.log("start linstening port 3000");
 });
