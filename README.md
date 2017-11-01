4c744d4349cf2ddf3aaf7984ea7ba3879d714c3a
toChaim <toChaim@gmail.com>
2017-11-01 11:04:27 -0700

    last commit before spliting into front and back ends.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # todo
-Basic todo app with users. Flexbox, React front end. Node, Express, MongoDb back end. 
+Front end for Basic todo app with users. Flexbox, React front end. Node, Express, MongoDb back end. 
---
+++ todo-api/index.js
@@ -0,0 +1,11 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+console.log("we are here. Now");
+
+ReactDOM.render(
+  <main id="main">
+    <h1>Hellow Chaim</h1>
+  </main>,
+  document.getElementById("root")
+);
